fba53edb02633d347a57a3e33350f0d75aa7953c
Kamil Szczepanik <kamilszczepanik94@gmail.com>
2023-01-09 19:28:28 +0100

    Initial version

--- src/main/java/com/kodilla/Main.java
+++ src/main/java/com/kodilla/Main.java
@@ -1,8 +1,1 @@
-package com.kodilla;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-    git config --global user.name "Kamil Szczepanik"
 
